d1f1388301eeb557a3aeda268cf7b175a8142109
VadymMintenko <vad.m1ntenko@gmail.com>
2023-04-08 14:03:20 +0300

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,62 @@
+import { ContactsList } from './ContactsList';
+import { ContactsForm } from './ContactsForm';
+import { Filter } from './Filter';
+import { nanoid } from 'nanoid';
+import { Container, ContactsListSContainer } from './ContactsForm.styled';
+import { useState, useEffect } from 'react';
+
 export const App = () => {
+  const [contacts, setContacts] = useState(
+    () =>
+      JSON.parse(window.localStorage.getItem('contacts')) ?? [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ]
+  );
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const handleSubmit = (values, { resetForm }) => {
+    const newContact = {
+      id: nanoid(),
+      name: values.name,
+      number: values.number,
+    };
+    if (contacts.map(contact => contact.name).includes(newContact.name)) {
+      alert(`Contact ${newContact.name} already exists.`);
+      return;
+    }
+    setContacts([...contacts, newContact]);
+    resetForm();
+  };
+
+  const searchContact = evt => {
+    setFilter(evt.target.value.toLowerCase());
+  };
+
+  const onDeleteContact = contactId => {
+    setContacts(contacts.filter(contact => contact.id !== contactId));
+
+    setFilter('');
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <ContactsListSContainer>
+        <ContactsForm handleSubmit={handleSubmit} />
+        <ContactsList
+          contacts={contacts}
+          filter={filter}
+          onDeleteContact={onDeleteContact}
+        />
+
+        <Filter filter={filter} searchContact={searchContact} />
+      </ContactsListSContainer>
+    </Container>
   );
 };
